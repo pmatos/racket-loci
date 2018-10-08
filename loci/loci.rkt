f369b981f03a76e91838e46cd74f851096f091f3
Paulo Matos <pmatos@linki.tools>
2018-10-08 13:39:38 +0200

    Remove unused file

--- loci/loci.rkt
+++
@@ -1,6 +0,0 @@
-#lang racket/base
-;; ---------------------------------------------------------------------------------------------------
-
-(require "private/locus-local.rkt")
-
-;(provide (all-from-out "private/locus-local.rkt"))
