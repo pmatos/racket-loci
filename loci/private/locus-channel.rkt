c0bf449429cccf743d6e584934954b3b6e2f9d6a
Paulo Matos <pmatos@linki.tools>
2018-10-11 10:07:47 +0200

    Setup locus channel as an input and output port.

--- loci/private/locus-channel.rkt
+++ loci/private/locus-channel.rkt
@@ -1,20 +1,28 @@
 #lang racket/base
 ;; ---------------------------------------------------------------------------------------------------
 
-(require racket/fasl
+(require racket/contract
+         racket/fasl
          racket/serialize)
 
 (provide
- make-locus-channel/child
- locus-channel
- locus-channel?
- locus-channel-put
- locus-channel-get
- locus-message-allowed?)
+ (contract-out
+  [make-locus-channel/child (-> locus-channel?)]
+  [locus-channel (input-port? output-port? . -> . locus-channel?)]
+  [locus-channel? (any/c . -> . boolean?)]
+  [locus-channel-put (locus-channel? locus-message-allowed? . -> . void?)]
+  [locus-channel-get (locus-channel? . -> . any/c)]
+  [locus-message-allowed? (any/c . -> . boolean?)]))
 
 ;; ---------------------------------------------------------------------------------------------------
 
-(struct locus-channel (in out))
+;; A locus channel consists of two file-streams to communicate with the locus
+;; in is connected to the locus stdout
+;; out is connected to the locus stdin
+;; therefore one reads from in and writes to out
+(struct locus-channel (in out)
+  #:property prop:input-port (struct-field-index in)
+  #:property prop:output-port (struct-field-index out))
 
 (define (make-locus-channel/child)
   (locus-channel (current-input-port) (current-output-port)))
